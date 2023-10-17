56f100c2a3bcc2876f544290b4b25058a209a61b
martytrajkoski1 <marti.trajkoski@students.finki.ukim.mk>
2023-10-17 20:00:47 +0200

    tutorialSplit

--- designmasterclass_backend/api/urls.py
+++ designmasterclass_backend/api/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from accounts.api.urls import tutorialphotoshop_router, tutorialillustrator_router
+from accounts.api.urls import tutorialillustrator_router, tutorialphotoshop_router
 from django.urls import path, include
 
 router = DefaultRouter()
 #tutorials
-router.registry.extend(tutorialphotoshop_router.registry)
 router.registry.extend(tutorialillustrator_router.registry)
+router.registry.extend(tutorialphotoshop_router.registry)
 
 urlpatterns = [
     path('', include(router.urls))
