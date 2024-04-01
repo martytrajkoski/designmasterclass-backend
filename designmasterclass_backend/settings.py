d35402ff3b27bbdcff90e3ede7bb8836b191257c
martytrajkoski <martytrajkoski@gmail.com>
2024-04-01 23:42:03 +0200

    db Updated

--- designmasterclass_backend/settings.py
+++ designmasterclass_backend/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'designmasterclass_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'sqlite.db',
     }
 }
 
